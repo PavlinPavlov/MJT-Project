d0f89addd29edf5f76bcb1d8d817e81cfb566b41
Pavlin Pavlov <888pavlin888@gmail.com>
2021-10-19 16:18:45 +0300

    Initial set up commit with skeleton

---
+++ src/main/java/tu/p_pavlov/mjt/project/MjtProjectApplication.java
@@ -0,0 +1,13 @@
+package tu.p_pavlov.mjt.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjtProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tu/p_pavlov/mjt/project/MjtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tu.p_pavlov.mjt.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
