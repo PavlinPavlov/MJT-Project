487492a3b22920703118f2ab07ae105d4de46f1e
Pavlin Pavlov <888pavlin888@gmail.com>
2021-10-20 16:21:10 +0300

    Item service

--- src/main/java/tu/p_pavlov/mjt/project/domain/service/ItemService.java
+++ src/main/java/tu/p_pavlov/mjt/project/domain/service/ItemService.java
@@ -1,9 +1,16 @@
 package tu.p_pavlov.mjt.project.domain.service;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import tu.p_pavlov.mjt.project.domain.model.Item;
 
 public interface ItemService {
 
     Mono<Item> save(Item itemToCreate);
+
+    Mono<Item> findById(String id);
+
+    Flux<Item> findAll();
+
+    Mono<Void> deleteById(String id);
 }
--- src/main/java/tu/p_pavlov/mjt/project/domain/service/impl/ItemServiceImpl.java
+++ src/main/java/tu/p_pavlov/mjt/project/domain/service/impl/ItemServiceImpl.java
@@ -1,6 +1,7 @@
 package tu.p_pavlov.mjt.project.domain.service.impl;
 
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import tu.p_pavlov.mjt.project.domain.model.Item;
 import tu.p_pavlov.mjt.project.domain.repository.ItemRepository;
@@ -18,5 +19,20 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public Mono<Item> save(Item itemToCreate) {
         return itemRepository.save(itemToCreate);
+    }
+
+    @Override
+    public Mono<Item> findById(String id) {
+        return itemRepository.findById(id);
+    }
+
+    @Override
+    public Flux<Item> findAll() {
+        return itemRepository.findAll();
+    }
+
+    @Override
+    public Mono<Void> deleteById(String id) {
+        return itemRepository.deleteById(id);
     }
 }
