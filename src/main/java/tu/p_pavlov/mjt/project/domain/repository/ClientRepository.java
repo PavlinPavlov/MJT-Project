e31a92c4e57c2355a5cbd46bde8b71f44f64efb1
Pavlin Pavlov <888pavlin888@gmail.com>
2021-10-20 10:29:10 +0300

    Basic repositories added

---
+++ src/main/java/tu/p_pavlov/mjt/project/domain/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package tu.p_pavlov.mjt.project.domain.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import tu.p_pavlov.mjt.project.domain.model.Client;
+
+@Repository
+public interface ClientRepository extends ReactiveMongoRepository<Client, String> {
+}
---
+++ src/main/java/tu/p_pavlov/mjt/project/domain/repository/PurchaseRepository.java
@@ -0,0 +1,9 @@
+package tu.p_pavlov.mjt.project.domain.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import tu.p_pavlov.mjt.project.domain.model.Purchase;
+
+@Repository
+public interface PurchaseRepository extends ReactiveMongoRepository<Purchase, String> {
+}
