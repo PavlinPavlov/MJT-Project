151b44da5a736c23fef030eadb7cce132a511356
Pavlin Pavlov <888pavlin888@gmail.com>
2021-10-23 23:07:57 +0300

    Fetched daat on purchase save

--- src/main/java/tu/p_pavlov/mjt/project/domain/service/impl/PurchaseServiceImpl.java
+++ src/main/java/tu/p_pavlov/mjt/project/domain/service/impl/PurchaseServiceImpl.java
@@ -5,26 +5,43 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import tu.p_pavlov.mjt.project.domain.PurchaseFilter;
-import tu.p_pavlov.mjt.project.domain.model.BaseDocument;
 import tu.p_pavlov.mjt.project.domain.model.Purchase;
+import tu.p_pavlov.mjt.project.domain.repository.ClientRepository;
+import tu.p_pavlov.mjt.project.domain.repository.ItemRepository;
 import tu.p_pavlov.mjt.project.domain.repository.PurchaseRepository;
 import tu.p_pavlov.mjt.project.domain.service.PurchaseService;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashSet;
 
 @Service
 public class PurchaseServiceImpl implements PurchaseService {
 
     private final PurchaseRepository purchaseRepository;
+    private final ItemRepository itemRepository;
+    private final ClientRepository clientRepository;
 
-    public PurchaseServiceImpl(PurchaseRepository purchaseRepository) {
+    public PurchaseServiceImpl(PurchaseRepository purchaseRepository, ItemRepository itemRepository, ClientRepository clientRepository) {
         this.purchaseRepository = purchaseRepository;
+        this.itemRepository = itemRepository;
+        this.clientRepository = clientRepository;
     }
 
     @Override
     public Mono<Purchase> save(Purchase purchaseToSave) {
-        return purchaseRepository.save(purchaseToSave);
+
+        return Flux.fromIterable(purchaseToSave.getItems())
+                .filterWhen(emptyItem -> itemRepository.existsById(emptyItem.getId()))
+                .flatMap(emptyItem -> itemRepository.findById(emptyItem.getId()))
+                .collectList()
+                .flatMap(fetchedItems -> {
+                    purchaseToSave.setItems(new HashSet<>(fetchedItems));
+                    return Mono.just(purchaseToSave);
+                })
+                .flatMap(purchase -> clientRepository.findById(purchase.getClient().getId()))
+                .flatMap(client -> {
+                    purchaseToSave.setClient(client);
+                    return purchaseRepository.save(purchaseToSave);
+                });
     }
 
     @Override
